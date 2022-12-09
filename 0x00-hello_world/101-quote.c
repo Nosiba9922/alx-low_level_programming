484d73cf5ae3037b19a6a1f7a873e07716198172
Nosiba9922 <nosibakhider9922@gmail.com>
2022-12-08 16:51:45 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <studio.h>
+
+
 /**
 * main - Entry point
-* Return: 1
+*
+* Return always 1 (Success)
 */
 int main(void)
 {
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	Write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
