9fd843abe81cee5a6ff15997d2f6d884172de581
Nosiba9922 <nosibakhider9922@gmail.com>
2022-12-12 15:44:31 -0800

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 	int c;
 	char i;
 
-	for (c = 0; c<= 9; c++)
-		putchar(c % 10 +'0');
+	for (c = 0; c <= 9; c++)
+		putchar(c % 10 + '0');
 
-	for (i = 'a'; i<='f'; i++)
+	for (i = 'a'; i <='f'; i++)
 		putchar(i);
 
 	putchar('\n');
