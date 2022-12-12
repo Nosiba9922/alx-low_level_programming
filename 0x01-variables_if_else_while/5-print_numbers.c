1ed21058f50a329cc905c72519682711f40e9ec7
Nosiba9922 <nosibakhider9922@gmail.com>
2022-12-12 14:51:04 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ *
+ * Return:Always 0
+ */
+int main(void)
+	char f = '0';
+
+	while (f <= '9')
+{
+	putchar(f);
+	f++;
+}
+putchar('\n');
+return (0);
+}
