8821208d18576836f181a1ccbcfdb3e7b44ff998
Nosiba9922 <nosibakhider9922@gmail.com>
2022-12-14 14:16:20 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		fopr (alpha = 'a'; alpha <= 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
