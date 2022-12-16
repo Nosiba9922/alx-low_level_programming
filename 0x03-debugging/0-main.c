1d110b3cf86d9c3f4a3bd60a7a9a1dce4c8f18a1
Nosiba9922 <nosibakhider9922@gmail.com>
2022-12-16 01:33:31 -0800

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function taht prints if integer is positive
+ * main - tests funtion that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
